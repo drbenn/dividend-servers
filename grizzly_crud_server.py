7943396d81311afaa37519ee996eae20e09681c2
ratlord <bennett.daniel@gmail.com>
2023-07-09 22:24:00 -0400

    crud register to db

--- grizzly_crud_server.py
+++ grizzly_crud_server.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_mysqldb import MySQL
 from flask_cors import CORS, cross_origin
 import random
+import jsonify
 import time
 from datetime import datetime as dt
 from datetime import time, timedelta
@@ -30,6 +31,13 @@ connection = psycopg2.connect(
     user="postgres",
     password="postgres",
     port="5432")
+
+grizzly_users_db_schema = {
+   "username": 0,
+   "email": 1,
+   "password": 2,
+   "join_date": 3
+}
 
 #cursor
 cur = connection.cursor()
@@ -69,15 +77,72 @@ def math():
 @app.route("/register", methods=['POST'])
 def insert_new_user() -> any:
   user_data = request.get_json()
-  # for ticker in tickers_to_add_to_db:
-  #    for ticker_info in all_current_us_tickers:
-  #       if ticker == ticker_info["symbol"]: 
-  print("USER REGISTER DATA")
-  print(user_data)
+  submitted_username = user_data["username"]
+  submitted_email = user_data["email"]
+  submitted_password = user_data["password"]
+  # Check if username or password is unique
   cur = connection.cursor()
-  cur.execute("INSERT INTO grizzly_users (username, email, password, join_date) values (%s, %s, %s, %s)", (user_data["username"], user_data["email"], user_data["password"], dt.now() ))
-  connection.commit()
-  cur.close()
-  return 'User registered successfully', 200
+  query = "SELECT username, email FROM grizzly_users"
+  cur.execute(query)
+  retrieved_db_data = cur.fetchall()
+  is_username_and_password_unique = True
+  error_message = None
+  for user in retrieved_db_data:
+    db_username = user[grizzly_users_db_schema["username"]]
+    db_email = user[grizzly_users_db_schema["email"]]
+    if submitted_username == db_username:
+      print('username already in use')
+      is_username_and_password_unique = False
+      error_message = 'Invalid request. Username already exists.'
+    elif submitted_email == db_email:
+      print('email already in use')
+      is_username_and_password_unique = False
+      error_message = 'Invalid request. Email already exists.'
+
+  if is_username_and_password_unique:
+    cur = connection.cursor()
+    cur.execute("INSERT INTO grizzly_users (username, email, password, join_date) values (%s, %s, %s, %s)", (submitted_username, submitted_email, submitted_password, dt.now() ))
+    connection.commit()
+    cur.close()
+    return 'User registered successfully', 200
+  else:
+    cur.close()
+    return error_message, 400
+  
+@app.route("/login", methods=['GET'])
+def insert_new_user() -> any:
+  user_data = request.get_json()
+  submitted_username = user_data["username"]
+  submitted_email = user_data["email"]
+  submitted_password = user_data["password"]
+  # Check if username or password is unique
+  cur = connection.cursor()
+  query = "SELECT username, email FROM grizzly_users"
+  cur.execute(query)
+  retrieved_db_data = cur.fetchall()
+  is_username_and_password_unique = True
+  error_message = None
+  for user in retrieved_db_data:
+    db_username = user[grizzly_users_db_schema["username"]]
+    db_email = user[grizzly_users_db_schema["email"]]
+    if submitted_username == db_username:
+      print('username already in use')
+      is_username_and_password_unique = False
+      error_message = 'Invalid request. Username already exists.'
+    elif submitted_email == db_email:
+      print('email already in use')
+      is_username_and_password_unique = False
+      error_message = 'Invalid request. Email already exists.'
+
+  if is_username_and_password_unique:
+    cur = connection.cursor()
+    cur.execute("INSERT INTO grizzly_users (username, email, password, join_date) values (%s, %s, %s, %s)", (submitted_username, submitted_email, submitted_password, dt.now() ))
+    connection.commit()
+    cur.close()
+    return 'User registered successfully', 200
+  else:
+    cur.close()
+    return error_message, 400
+  
 
 if __name__ == "__main__": app.run()
